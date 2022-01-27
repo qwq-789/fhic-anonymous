270ec42a7f5359c3f91a7d9f2588b0dace2a8b51
qwq-789 <elfrain520@gmail.com>
2022-01-27 11:44:27 +0800

    v3 here is so many error

--- src/main.js
+++ src/main.js
@@ -1,1 +1,9 @@
 import './style.css'
+import './index.js'
+import './elementConfig.js'
+import './del.js'
+
+import fontawesome from '@fortawesome/fontawesome'
+import { faTimesCircle, faReply } from '@fortawesome/fontawesome-free-solid'
+
+fontawesome.library.add(faTimesCircle, faReply)
